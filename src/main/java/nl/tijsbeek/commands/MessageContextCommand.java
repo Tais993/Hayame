d7c66319957ef0b7f7f0b0c4b2b6923feec0f213
nltbee <Tijs.Beek@vanderlande.com>
2022-03-04 12:15:46 +0100

    Added interface for basic context commands

---
+++ src/main/java/nl/tijsbeek/commands/MessageContextCommand.java
@@ -0,0 +1,9 @@
+package nl.tijsbeek.commands;
+
+import net.dv8tion.jda.api.events.interaction.command.MessageContextInteractionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public interface MessageContextCommand extends InteractionCommand {
+
+    void onMessageContextInteraction(@NotNull MessageContextInteractionEvent event);
+}
---
+++ src/main/java/nl/tijsbeek/commands/UserContextCommand.java
@@ -0,0 +1,9 @@
+package nl.tijsbeek.commands;
+
+import net.dv8tion.jda.api.events.interaction.command.UserContextInteractionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public interface UserContextCommand extends InteractionCommand {
+
+    void onUserContextInteraction(@NotNull UserContextInteractionEvent event);
+}
