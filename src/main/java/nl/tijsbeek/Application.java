8e40b870982b79db4ea807e88c727ac467fa0c8d
nltbee <Tijs.Beek@vanderlande.com>
2022-03-04 16:45:08 +0100

    Start main method

--- src/main/java/nl/tijsbeek/Application.java
+++ src/main/java/nl/tijsbeek/Application.java
@@ -1,5 +1,13 @@
 package nl.tijsbeek;
 
 public class Application {
+    public static void main(final String[] args) {
+        if (args.length <= 0) {
+            throw new IllegalArgumentException("Missing token!");
+        }
 
+        String token = args[0];
+
+
+    }
 }
