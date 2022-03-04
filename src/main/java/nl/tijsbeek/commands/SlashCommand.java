102917b3abb5e0a33e3350bb0ca3e0b113096014
nltbee <Tijs.Beek@vanderlande.com>
2022-03-04 12:16:01 +0100

    Added interface for basic slash commands

---
+++ src/main/java/nl/tijsbeek/commands/SlashCommand.java
@@ -0,0 +1,14 @@
+package nl.tijsbeek.commands;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import org.jetbrains.annotations.NotNull;
+
+public interface SlashCommand extends InteractionCommand {
+
+    @NotNull
+    @Override
+    SlashCommandData getData();
+
+    void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event);
+}
