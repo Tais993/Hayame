2674932ca5476fb653d50d92592eb53565342710
Tais993 <tijs@familiebeek.eu>
2022-03-05 13:14:20 +0100

    Added EventList

---
+++ src/main/java/nl/tijsbeek/commands/system/EventList.java
@@ -0,0 +1,20 @@
+package nl.tijsbeek.commands.system;
+
+import nl.tijsbeek.commands.CustomEventListener;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventList {
+
+    @NotNull
+    public static List<CustomEventListener> getCommands() {
+        List<CustomEventListener> commands = new ArrayList<>(20);
+
+        // TEMPORARY PLACEHOLDER AGAINST WARNINGS
+        commands.contains(null);
+
+        return commands;
+    }
+}
