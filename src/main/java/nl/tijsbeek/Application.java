2723d8f4e38607a5b5761e6053186ec591cfca1e
Tais993 <tijs@familiebeek.eu>
2022-03-05 13:15:50 +0100

    Updated main method

--- src/main/java/nl/tijsbeek/Application.java
+++ src/main/java/nl/tijsbeek/Application.java
@@ -1,13 +1,34 @@
 package nl.tijsbeek;
 
-public class Application {
-    public static void main(final String[] args) {
+import net.dv8tion.jda.api.JDABuilder;
+import nl.tijsbeek.commands.system.CommandHandler;
+import nl.tijsbeek.commands.system.EventHandler;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.security.auth.login.LoginException;
+
+public final class Application {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    @Contract(pure = true)
+    private Application() {}
+
+    public static void main(@NotNull final String @NotNull [] args) throws LoginException {
         if (args.length <= 0) {
             throw new IllegalArgumentException("Missing token!");
         }
 
         String token = args[0];
 
+        CommandHandler commandHandler = new CommandHandler();
+        EventHandler eventHandler = new EventHandler();
 
+        JDABuilder.create(token, eventHandler.getGatewayIntents())
+                .enableCache(eventHandler.getCacheFlags())
+                .addEventListeners(commandHandler, eventHandler)
+                .build();
     }
 }
