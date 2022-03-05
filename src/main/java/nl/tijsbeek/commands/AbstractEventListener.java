ae6550c3463e97787a4fcdc1d82ab4652ae8c7c4
Tais993 <tijs@familiebeek.eu>
2022-03-05 13:00:43 +0100

    Added abstraction for event listener

---
+++ src/main/java/nl/tijsbeek/commands/AbstractEventListener.java
@@ -0,0 +1,39 @@
+package nl.tijsbeek.commands;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.UnmodifiableView;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class AbstractEventListener extends ListenerAdapter implements CustomEventListener {
+    private final Collection<GatewayIntent> requiredIntents = new ArrayList<>();
+    private final Collection<CacheFlag> requiredCacheFlags = new ArrayList<>();
+
+    protected final void addRequiredIntents(@NotNull final GatewayIntent... intents) {
+        requiredIntents.addAll(List.of(intents));
+    }
+
+    protected final void addRequiredCacheFlags(@NotNull final CacheFlag... cacheFlags) {
+        requiredCacheFlags.addAll(List.of(cacheFlags));
+    }
+
+    @NotNull
+    @Override
+    @UnmodifiableView
+    public Collection<CacheFlag> getRequiredCacheFlags() {
+        return Collections.unmodifiableCollection(requiredCacheFlags);
+    }
+
+    @NotNull
+    @Override
+    @UnmodifiableView
+    public Collection<GatewayIntent> getRequiredIntents() {
+        return Collections.unmodifiableCollection(requiredIntents);
+    }
+}
