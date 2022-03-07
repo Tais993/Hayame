7c6d3cda9a35eac4d0b2e71d14effb769b03975f
Tais993 <tijs@familiebeek.eu>
2022-03-07 23:46:11 +0100

    Added documentation to CustomEventListener

--- src/main/java/nl/tijsbeek/discord/events/CustomEventListener.java
+++ src/main/java/nl/tijsbeek/discord/events/CustomEventListener.java
@@ -8,12 +8,27 @@ import org.jetbrains.annotations.UnmodifiableView;
 
 import java.util.Collection;
 
+/**
+ * Adds event support for classes.
+ *
+ * @see AbstractEventListener
+ */
 public interface CustomEventListener extends EventListener {
 
+    /**
+     * Returns all the {@link GatewayIntent GatewayIntents} this class requires for its events.
+     *
+     * @return a {@link Collection} of {@link GatewayIntent GatewayIntents}
+     */
     @NotNull
     @UnmodifiableView
     Collection<GatewayIntent> getRequiredIntents();
 
+    /**
+     * Returns all the {@link CacheFlag CacheFlags} this class requires for its events.
+     *
+     * @return a {@link Collection} of {@link CacheFlag CacheFlags}
+     */
     @NotNull
     @UnmodifiableView
     Collection<CacheFlag> getRequiredCacheFlags();
