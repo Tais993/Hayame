6137c828a99068bf028cbd2a40f9463a1dd0a609
Tais993 <tijs@familiebeek.eu>
2022-03-07 21:07:39 +0100

    Added documentation to MessageContextCommand and UserContextCommand

--- src/main/java/nl/tijsbeek/discord/commands/MessageContextCommand.java
+++ src/main/java/nl/tijsbeek/discord/commands/MessageContextCommand.java
@@ -1,9 +1,20 @@
 package nl.tijsbeek.discord.commands;
 
 import net.dv8tion.jda.api.events.interaction.command.MessageContextInteractionEvent;
+import nl.tijsbeek.discord.commands.abstractions.AbstractInteractionCommand;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Represents a Discord message context-command.
+ *
+ * @see AbstractInteractionCommand
+ */
 public interface MessageContextCommand extends InteractionCommand {
 
+    /**
+     * The {@link MessageContextInteractionEvent} that triggers when someone runs a message context-command.
+     *
+     * @param event the {@link MessageContextInteractionEvent}
+     */
     void onMessageContextInteraction(@NotNull MessageContextInteractionEvent event);
 }
--- src/main/java/nl/tijsbeek/discord/commands/UserContextCommand.java
+++ src/main/java/nl/tijsbeek/discord/commands/UserContextCommand.java
@@ -1,9 +1,20 @@
 package nl.tijsbeek.discord.commands;
 
 import net.dv8tion.jda.api.events.interaction.command.UserContextInteractionEvent;
+import nl.tijsbeek.discord.commands.abstractions.AbstractInteractionCommand;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Represents a Discord user context-command.
+ *
+ * @see AbstractInteractionCommand
+ */
 public interface UserContextCommand extends InteractionCommand {
 
+    /**
+     * The {@link UserContextInteractionEvent} that triggers when someone runs a user context-command.
+     *
+     * @param event the {@link UserContextInteractionEvent}
+     */
     void onUserContextInteraction(@NotNull UserContextInteractionEvent event);
 }
