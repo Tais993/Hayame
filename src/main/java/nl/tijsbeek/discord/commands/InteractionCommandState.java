d1f6a81ae5864d6acf13088442c091ff78ef14c5
Tais993 <tijs@familiebeek.eu>
2022-03-07 20:52:07 +0100

    Added documentation to InteractionCommand, InteractionCommandState and InteractionCommandVisibility

--- src/main/java/nl/tijsbeek/discord/commands/InteractionCommandState.java
+++ src/main/java/nl/tijsbeek/discord/commands/InteractionCommandState.java
@@ -1,5 +1,8 @@
 package nl.tijsbeek.discord.commands;
 
+/**
+ * The state of the command, when set to {@link #DISABLED} the command won't be registered on Discord.
+ */
 public enum InteractionCommandState {
     ENABLED,
     DISABLED
--- src/main/java/nl/tijsbeek/discord/commands/InteractionCommandVisibility.java
+++ src/main/java/nl/tijsbeek/discord/commands/InteractionCommandVisibility.java
@@ -1,7 +1,16 @@
 package nl.tijsbeek.discord.commands;
 
+import nl.tijsbeek.discord.commands.abstractions.AbstractInteractionCommand;
+
+/**
+ * The visibility of the command.
+ */
 public enum InteractionCommandVisibility {
     GLOBAL,
     GUILD_ONLY,
+
+    /**
+     * When set to private, use {@link InteractionCommand#getEnabledGuilds()}, or {@link AbstractInteractionCommand#addEnabledGuilds(Long...)} to register guilds.
+     */
     PRIVATE
 }
