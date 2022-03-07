6c15715f9d4bf8d8a0194cf50d2f5381c083ffbd
Tais993 <tijs@familiebeek.eu>
2022-03-07 22:56:36 +0100

    Added documentation to ComponentDatabase

--- src/main/java/nl/tijsbeek/discord/components/ComponentDatabase.java
+++ src/main/java/nl/tijsbeek/discord/components/ComponentDatabase.java
@@ -8,6 +8,7 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import nl.tijsbeek.config.Config;
 import org.flywaydb.core.Flyway;
+import org.intellij.lang.annotations.Language;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -26,6 +27,9 @@ import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * The database handler for components.
+ */
 public final class ComponentDatabase {
     private static final Logger logger = LoggerFactory.getLogger(ComponentDatabase.class);
     private static final String DB_SCHEMA_BOT = "discordbot";
@@ -51,6 +55,13 @@ public final class ComponentDatabase {
 
     }
 
+    /**
+     * Writes an array of arguments to a CSV string.
+     *
+     * @param arguments an array of arguments
+     *
+     * @return the arguments as a CSV string
+     */
     @Nullable
     private static String argumentsToCsvString(@NotNull final String... arguments) {
         StringWriter writer = new StringWriter();
@@ -64,9 +75,16 @@ public final class ComponentDatabase {
         }
     }
 
+    /**
+     * Writes a CSV string to a {@link List} of arguments
+     *
+     * @param csv a CSV string
+     *
+     * @return the CSV String as a {@link List} of arguments
+     */
     @NotNull
     @Unmodifiable
-    private static List<String> csvStringToArguments(@NotNull final String csv) {
+    private static List<String> csvStringToArguments(@Language("csv") @NotNull final String csv) {
         try (CSVReader reader = new CSVReader(new StringReader(csv))) {
             return List.of(reader.readNext());
         } catch (IOException | CsvValidationException e) {
@@ -75,8 +93,16 @@ public final class ComponentDatabase {
         }
     }
 
+    /**
+     * Creates an ID, and inserts it into the DB
+     *
+     * @param expirationDate the date for when the component should expire
+     * @param arguments the arguments of the ID, can be empty
+     *
+     * @return the ID
+     */
     @Nullable
-    public String createId(@NotNull final LocalDateTime localDateTime, String... arguments) {
+    public String createId(@Nullable final LocalDateTime expirationDate, @NotNull String... arguments) {
         try (Connection connection = dataSource.getConnection();
              PreparedStatement statement = connection.prepareStatement("""
                      INSERT INTO discordbot.component (expire_date, arguments)
@@ -84,7 +110,7 @@ public final class ComponentDatabase {
                      RETURNING id
                      """)) {
 
-            statement.setObject(1, localDateTime);
+            statement.setObject(1, expirationDate);
             statement.setObject(2, argumentsToCsvString(arguments));
 
             statement.execute();
@@ -100,6 +126,11 @@ public final class ComponentDatabase {
     }
 
 
+    /**
+     * Removes component information by its ID
+     *
+     * @param id the ID to remove the component from
+     */
     public void remove(@NotNull final String id) {
         try (Connection connection = dataSource.getConnection();
              PreparedStatement statement = connection.prepareStatement("""
@@ -117,6 +148,13 @@ public final class ComponentDatabase {
     }
 
 
+    /**
+     * Retrieves component information based of its ID.
+     *
+     * @param id the ID of the component to retrieve
+     *
+     * @return the {@link ComponentEntity}
+     */
     @NotNull
     @Contract("_ -> new")
     public ComponentEntity retrieveComponentEntity(@NotNull final String id) {
