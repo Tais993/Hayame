1b96b148973c0a6989070fbef3b37e212f35470f
Tais993 <tijs@familiebeek.eu>
2022-03-07 21:52:31 +0100

    Added documentation to AbstractSlashCommand

--- src/main/java/nl/tijsbeek/discord/commands/abstractions/AbstractSlashCommand.java
+++ src/main/java/nl/tijsbeek/discord/commands/abstractions/AbstractSlashCommand.java
@@ -1,5 +1,6 @@
 package nl.tijsbeek.discord.commands.abstractions;
 
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 import nl.tijsbeek.discord.commands.InteractionCommandState;
@@ -7,6 +8,14 @@ import nl.tijsbeek.discord.commands.InteractionCommandVisibility;
 import nl.tijsbeek.discord.commands.SlashCommand;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * An abstraction of {@link SlashCommand} with the intention to improve developer experience.
+ * <p/>
+ * This implements methods like {@link #getData()}, {@link #getVisibility()} and {@link #getState()} for you. Their values are
+ * inserted within the constructor.
+ * <p/>
+ * Methods like {@link #addEnabledGuilds(Long...)}, {@link #addRequiredBotPermission(Permission...)} and {@link #addRequiredUserPermission(Permission...)} exist to improve the experience.
+ */
 public abstract class AbstractSlashCommand extends AbstractInteractionCommand implements SlashCommand {
     private final SlashCommandData commandData;
 
@@ -24,6 +33,6 @@ public abstract class AbstractSlashCommand extends AbstractInteractionCommand im
 
     @Override
     public void onCommandAutoCompleteInteractionEvent(@NotNull CommandAutoCompleteInteractionEvent event) {
-        
+
     }
 }
