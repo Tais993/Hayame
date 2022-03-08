abae861674258bf3aa3664ce45ad753cf1dc0cf1
Tais993 <tijs@familiebeek.eu>
2022-03-08 10:00:34 +0100

    Added documentation to StreamUtils

--- src/main/java/nl/tijsbeek/utils/StreamUtils.java
+++ src/main/java/nl/tijsbeek/utils/StreamUtils.java
@@ -5,8 +5,19 @@ import org.jetbrains.annotations.NotNull;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+
+/**
+ * Contains some utils for {@link Stream Streams}.
+ */
 public final class StreamUtils {
 
+    /**
+     * Collects the {@link Stream} of {@link String} within {@code ()} with {@code ,} as the delimiter.
+     *
+     * @param stream the {@link Stream} to collect
+     *
+     * @return the joined {@link String}
+     */
     @NotNull
     public static String toJoinedString(@NotNull final Stream<String> stream) {
         return stream.collect(Collectors.joining(",", "(", ")"));
