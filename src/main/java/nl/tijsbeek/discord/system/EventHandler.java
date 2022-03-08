811206e221bb87b9978dec58f765cf9fc94b9f3b
Tais993 <tijs@familiebeek.eu>
2022-03-08 09:54:38 +0100

    Added documentation to EventHandler

--- src/main/java/nl/tijsbeek/discord/system/EventHandler.java
+++ src/main/java/nl/tijsbeek/discord/system/EventHandler.java
@@ -13,14 +13,27 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * Handles all {@link CustomEventListener CustomEventListeners}.
+ */
 public class EventHandler implements EventListener {
     private final List<CustomEventListener> listeners;
 
+    /**
+     * Creates an instance based of {@link ListenersList#getEventListeners()}.
+     *
+     * @param listenersList the {@link ListenersList} to take the {@link CustomEventListener CustomEventListeners} from
+     */
     @Contract(pure = true)
     public EventHandler(@NotNull final ListenersList listenersList) {
         listeners = listenersList.getEventListeners();
     }
 
+    /**
+     * Returns the {@link CacheFlag CacheFlags} as a {@link Set} based of {@link CustomEventListener#getRequiredCacheFlags()}.
+     *
+     * @return the {@link Set} of {@link CacheFlag CacheFlags}
+     */
     @NotNull
     @Contract(pure = true)
     public final Set<CacheFlag> getCacheFlags() {
@@ -30,6 +43,11 @@ public class EventHandler implements EventListener {
                 .collect(Collectors.toSet());
     }
 
+    /**
+     * Returns the {@link GatewayIntent GatewayIntents} as a {@link Set} based of {@link CustomEventListener#getRequiredIntents()}.
+     *
+     * @return the {@link Set} of {@link GatewayIntent GatewayIntents}
+     */
     @NotNull
     @Contract(pure = true)
     public final Set<GatewayIntent> getGatewayIntents() {
@@ -40,9 +58,13 @@ public class EventHandler implements EventListener {
     }
 
 
+    /**
+     * Forwards the given {@link GenericEvent} to all {@link CustomEventListener CustomEventListeners}.
+     *
+     * @param event the {@link GenericEvent} to forward
+     */
     @Override
     public void onEvent(@NotNull GenericEvent event) {
         listeners.forEach(listener -> listener.onEvent(event));
     }
-
 }
