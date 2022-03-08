2866a8e0526224fb1d97867971b16d0d5f8e9036
Tais993 <tijs@familiebeek.eu>
2022-03-08 10:02:31 +0100

    Added documentation to Application

--- src/main/java/nl/tijsbeek/Application.java
+++ src/main/java/nl/tijsbeek/Application.java
@@ -1,5 +1,6 @@
 package nl.tijsbeek;
 
+import net.dv8tion.jda.api.JDABuilder;
 import nl.tijsbeek.config.Config;
 import nl.tijsbeek.discord.components.ComponentDatabase;
 import org.jetbrains.annotations.Contract;
@@ -10,12 +11,23 @@ import org.slf4j.LoggerFactory;
 import javax.security.auth.login.LoginException;
 import java.io.IOException;
 
+/**
+ * Contains the entry point of the application.
+ */
 public final class Application {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     @Contract(pure = true)
     private Application() {}
 
+    /**
+     * The entry point.
+     *
+     * @param args a {@link String} array
+     *
+     * @throws LoginException {@link JDABuilder#build()}
+     * @throws IOException {@link Config#loadInstance(String)}
+     */
     public static void main(@NotNull final String @NotNull [] args) throws LoginException, IOException {
         if (args.length < 1) {
             throw new IllegalArgumentException("Missing config location!");
