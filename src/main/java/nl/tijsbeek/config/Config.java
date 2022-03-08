b827d29670e66012a9afbbf19b5baf6faf9fbdcb
Tais993 <tijs@familiebeek.eu>
2022-03-08 10:06:47 +0100

    Added documentation to Config

--- src/main/java/nl/tijsbeek/config/Config.java
+++ src/main/java/nl/tijsbeek/config/Config.java
@@ -9,6 +9,9 @@ import org.jetbrains.annotations.NotNull;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * Contains the config's variables
+ */
 public class Config {
 
     private final String discordToken;
@@ -16,6 +19,14 @@ public class Config {
     private final String databaseUsername;
     private final String databasePassword;
 
+    /**
+     * Creates an instance
+     *
+     * @param discordToken the discord-token
+     * @param databasePort the port of the DB
+     * @param databaseUsername the username of the DB
+     * @param databasePassword the password of the DB
+     */
     @JsonCreator
     @Contract(pure = true)
     public Config(@JsonProperty("discord_token") String discordToken,
@@ -29,22 +40,47 @@ public class Config {
         this.databasePassword = databasePassword;
     }
 
+    /**
+     * The Discord token
+     * @return the Discord token
+     */
     public String getDiscordToken() {
         return discordToken;
     }
 
+    /**
+     * The port of the DB
+     * @return the port of the DB
+     */
     public String getDatabasePort() {
         return databasePort;
     }
 
+    /**
+     * The username of the DB
+     * @return the username of the DB
+     */
     public String getDatabaseUsername() {
         return databaseUsername;
     }
 
+    /**
+     * The password of the DB
+     * @return the password of the DB
+     */
     public String getDatabasePassword() {
         return databasePassword;
     }
 
+    /**
+     * Creates an instance based of the URL of the file.
+     *
+     * @param configUrl a valid URL
+     *
+     * @return the Config
+     *
+     * @throws IOException if a low-level I/O error occurs.
+     */
     @NotNull
     public static Config loadInstance(@NotNull final String configUrl) throws IOException {
         return new ObjectMapper().readValue(new File(configUrl), Config.class);
