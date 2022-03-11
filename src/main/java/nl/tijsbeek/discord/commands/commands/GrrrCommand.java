b9730b4a633fd94cbd939630f07744599095990b
Tais993 <tijs@familiebeek.eu>
2022-03-11 12:55:17 +0100

    Updated GrrrCommand so it can be used for testing

--- src/main/java/nl/tijsbeek/discord/commands/commands/GrrrCommand.java
+++ src/main/java/nl/tijsbeek/discord/commands/commands/GrrrCommand.java
@@ -1,24 +1,35 @@
 package nl.tijsbeek.discord.commands.commands;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import nl.tijsbeek.discord.commands.InteractionCommandState;
 import nl.tijsbeek.discord.commands.InteractionCommandVisibility;
 import nl.tijsbeek.discord.commands.abstractions.AbstractSlashCommand;
 import org.jetbrains.annotations.NotNull;
 
+import java.time.LocalDateTime;
+
 public class GrrrCommand extends AbstractSlashCommand {
+
     public GrrrCommand() {
         super(Commands.slash("grrrrr", "aa"), InteractionCommandVisibility.GUILD_ONLY, InteractionCommandState.ENABLED);
-
     }
 
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
-        event.reply("grrrr").queue();
+        event.reply("grrrr")
+                .addActionRow(Button.success(componentDatabase.createId(getName(), getType().getId(), LocalDateTime.of(2022, 3, 12, 1, 1)), "a"))
+                .queue();
 
         for (int i = 0; i < 20000; i++) {
             System.out.println("a");
         }
+    }
+
+    @Override
+    public void onButtonInteraction(@NotNull ButtonInteractionEvent event) {
+        event.editMessage("Aa").queue();
     }
 }
