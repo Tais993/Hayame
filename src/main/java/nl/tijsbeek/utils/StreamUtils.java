5cea684cbf7eaec206a2eed52736729a153f6ab7
Tais993 <tijs@familiebeek.eu>
2022-03-11 10:43:50 +0100

    Added StreamUtils#toJoinedString(Stream<String>, String delimiter)
    Allows a Stream to be joined by custom delimiter

--- src/main/java/nl/tijsbeek/utils/StreamUtils.java
+++ src/main/java/nl/tijsbeek/utils/StreamUtils.java
@@ -22,4 +22,17 @@ public final class StreamUtils {
     public static String toJoinedString(@NotNull final Stream<String> stream) {
         return stream.collect(Collectors.joining(",", "(", ")"));
     }
+
+    /**
+     * Collects the {@link Stream} of {@link String} within {@code ()} with {@code ,} as the delimiter.
+     *
+     * @param stream the {@link Stream} to collect
+     * @param delimiter the delimiter
+     *
+     * @return the joined {@link String}
+     */
+    @NotNull
+    public static String toJoinedString(@NotNull final Stream<String> stream, @NotNull final String delimiter) {
+        return stream.collect(Collectors.joining(delimiter, "", ""));
+    }
 }
