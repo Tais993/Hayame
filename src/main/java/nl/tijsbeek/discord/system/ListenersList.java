0e8c8749290878581500e82d30923f0c848e78ab
nltbee <Tijs.Beek@vanderlande.com>
2022-04-06 17:06:41 +0200

    Implements the coin-flip command

--- src/main/java/nl/tijsbeek/discord/system/ListenersList.java
+++ src/main/java/nl/tijsbeek/discord/system/ListenersList.java
@@ -4,6 +4,7 @@ import nl.tijsbeek.discord.commands.InteractionCommand;
 import nl.tijsbeek.discord.commands.commands.context.message.AaaaaMCommand;
 import nl.tijsbeek.discord.commands.commands.context.user.AaaaaUCommand;
 import nl.tijsbeek.discord.commands.commands.slash.AskQuestionCommand;
+import nl.tijsbeek.discord.commands.commands.slash.CoinFlip;
 import nl.tijsbeek.discord.commands.commands.slash.GrrrCommand;
 import nl.tijsbeek.discord.commands.commands.slash.TestCommand;
 import nl.tijsbeek.discord.events.CustomEventListener;
@@ -41,6 +42,7 @@ public class ListenersList {
         commands.add(new AaaaaMCommand());
         commands.add(new AaaaaUCommand());
         commands.add(new AskQuestionCommand());
+        commands.add(new CoinFlip());
 
 
         this.eventListeners = eventListeners;
