1f958ee00a01cad9c411c24ccc8b07c06697da60
nltbee <Tijs.Beek@vanderlande.com>
2022-05-12 15:48:50 +0200

    Removed "command-type" from code, been replaced with prefix in the listener's ID

--- src/main/java/nl/tijsbeek/discord/components/ComponentEntity.java
+++ src/main/java/nl/tijsbeek/discord/components/ComponentEntity.java
@@ -15,7 +15,6 @@ public class ComponentEntity {
 
     private final String id;
     private final String listenerId;
-    private final Command.Type commandType;
     private final LocalDateTime expireDate;
     private final List<String> arguments;
 
@@ -28,10 +27,9 @@ public class ComponentEntity {
      * @param arguments a list of all arguments
      */
     @Contract(pure = true)
-    public ComponentEntity(@NotNull final String id, @Nullable final String listenerId, final int commandType, @Nullable final LocalDateTime expireDate, @NotNull final List<String> arguments) {
+    public ComponentEntity(@NotNull final String id, @Nullable final String listenerId, @Nullable final LocalDateTime expireDate, @NotNull final List<String> arguments) {
         this.id = Objects.requireNonNull(id, "The given id cannot be null");
         this.listenerId = listenerId;
-        this.commandType = Command.Type.fromId(commandType);
         this.expireDate = expireDate;
         this.arguments = Collections.unmodifiableList(Objects.requireNonNull(arguments, "The given arguments cannot be null"));
     }
@@ -44,10 +42,9 @@ public class ComponentEntity {
      * @param arguments a list of all arguments
      */
     @Contract(pure = true)
-    public ComponentEntity(@Nullable final String listenerId, final int commandType, @Nullable final LocalDateTime expireDate, @NotNull final List<String> arguments) {
+    public ComponentEntity(@Nullable final String listenerId, @Nullable final LocalDateTime expireDate, @NotNull final List<String> arguments) {
         this.id = null;
         this.listenerId = listenerId;
-        this.commandType = Command.Type.fromId(commandType);
         this.expireDate = expireDate;
         this.arguments = Collections.unmodifiableList(Objects.requireNonNull(arguments, "The given arguments cannot be null"));
     }
@@ -62,7 +59,7 @@ public class ComponentEntity {
     @NotNull
     @Contract("_ -> new")
     public static ComponentEntity empty(@NotNull final String id) {
-        return new ComponentEntity(id, null, 0, null,Collections.emptyList());
+        return new ComponentEntity(id, null, null,Collections.emptyList());
     }
 
     /**
@@ -83,16 +80,6 @@ public class ComponentEntity {
     @Nullable
     public String getListenerId() {
         return listenerId;
-    }
-
-    /**
-     * The {@link Command.Type} if there is any for the listener.
-     *
-     * @return the {@link Command.Type}
-     */
-    @Nullable
-    public Command.Type getCommandType() {
-        return commandType;
     }
 
     /**
