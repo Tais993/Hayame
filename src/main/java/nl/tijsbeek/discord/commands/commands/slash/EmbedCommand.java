ac88deb9bbf9313e89b09c27981dcb69eb0f77ca
Tijs <tijs@familiebeek.eu>
2022-06-27 12:29:40 +0200

    Updated JDA to alpha 12

--- src/main/java/nl/tijsbeek/discord/commands/commands/slash/EmbedCommand.java
+++ src/main/java/nl/tijsbeek/discord/commands/commands/slash/EmbedCommand.java
@@ -12,7 +12,7 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
-import net.dv8tion.jda.api.interactions.components.text.Modal;
+import net.dv8tion.jda.api.interactions.components.Modal;
 import net.dv8tion.jda.api.interactions.components.text.TextInput;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 import net.dv8tion.jda.api.interactions.modals.ModalMapping;
